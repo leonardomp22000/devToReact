4882701347c8bb43f01d52afb0882334a9dc6abe
Leonardo <leonardomp2200@gmail.com>
2025-03-29 14:57:20 -0600

    Creation of subcomponents Button, searchBar and addition of responsive design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,34 @@
+import Button from "./Button";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 export default function NavBar() {
   return (
-    <nav>
-      <h1 className="border border-red-600">Esta es mi navBar</h1>
+    <nav className="flex justify-between bg-white p-2.5">
+      <div className="flex gap-2 md:flex-2/3">
+        <div className="md:hidden">
+          <Button className="!p-1">
+            <Bars3Icon width={30} height={30} />
+          </Button>
+        </div>
+        <Image
+          width={50}
+          height={50}
+          alt="Logo"
+          src="https://media2.dev.to/dynamic/image/quality=100/https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
+        ></Image>
+        <div className="hidden w-full md:block">
+          <SearchBar />
+        </div>
+      </div>
+
+      <div className="flex justify-end gap-2.5 md:flex-1/3">
+        <div className="hidden md:block">
+          <Button variant="text">Log in</Button>
+        </div>
+
+        <Button variant="outlined">Create Account</Button>
+      </div>
     </nav>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
-    <div className="mx-auto max-w-6xl px-4">
+    <div className="mx-auto max-w-7xl">
       <NavBar></NavBar>
 
-      <div className="mt-2 sm:flex sm:gap-2.5">
+      <div className="mt-2 p-4 sm:flex sm:gap-2.5">
         <aside className="hidden border border-amber-600 md:block md:flex-1/3">
           {" "}
           {/**Este aside es el menu hamburguesa */}
@@ -14,7 +14,7 @@ export default function Home() {
           <h3>Este es mi main</h3>
         </main>
 
-        <aside className="hidden border border-amber-600 lg:flex lg:flex-1/3">
+        <aside className="hidden border border-amber-600 lg:flex lg:flex-2/3">
           {" "}
           {/**Este aside si desaparece por completo */}
           <h2>Este es mi aside derecho</h2>
