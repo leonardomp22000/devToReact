9e67fe599f946d4ea265061c695d62fed1cd25b4
Leonardo <leonardomp2200@gmail.com>
2025-04-01 20:58:00 -0600

    Creation of login card done, and correction of responsive main design

---
+++ src/components/CardBase.jsx
@@ -0,0 +1,5 @@
+export default function CardBase({ children }) {
+  return (
+    <section className="w-full rounded-md bg-white p-4">{children}</section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import NavBar from "@/components/NavBar";
+import CardLogIn from "@/components/CardLogIn";
 export default function Home() {
   return (
     <div className="mx-auto max-w-7xl">
       <NavBar></NavBar>
 
       <div className="mt-2 p-4 sm:flex sm:gap-2.5">
-        <aside className="hidden border border-amber-600 md:block md:flex-1/3">
-          {" "}
+        <aside className="hidden md:block md:grow md:basis-56">
           {/**Este aside es el menu hamburguesa */}
-          <h2>Este es mi aside izquierdo</h2>
+          <CardLogIn></CardLogIn>-
         </aside>
-        <main className="w-full border border-amber-600">
+        <main className="flex-8/12 border border-amber-600 lg:flex-6/12">
           <h3>Este es mi main</h3>
         </main>
 
-        <aside className="hidden border border-amber-600 lg:flex lg:flex-2/3">
+        <aside className="hidden border border-amber-600 lg:flex lg:basis-1/4">
           {" "}
           {/**Este aside si desaparece por completo */}
           <h2>Este es mi aside derecho</h2>
