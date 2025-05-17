b632dbd6e87b0190c2edd924700b7ceb88f16fad
Leonardo <leonardomp2200@gmail.com>
2025-05-17 14:13:53 -0600

    delete posts function done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,28 +1,64 @@
 import CardBase from "./CardBase";
 import Image from "next/image";
 import Button from "./Button";
-import { ChatBubbleLeftIcon, BookmarkIcon } from "@heroicons/react/24/outline";
+import {
+  ChatBubbleLeftIcon,
+  BookmarkIcon,
+  XMarkIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/outline";
+
 export default function Post({
   creationDate,
   userPicture,
   userName,
   mainText,
   body,
+  postID,
+  onDelete,
 }) {
+  const handleClose = () => {
+    onDelete(postID);
+    return;
+  };
+  const handleEdit = () => {
+    console.log("EDIT");
+    return;
+  };
   return (
-    <CardBase className="!p-1 md:p-4">
-      <div className="flex items-center gap-2">
-        <Image
-          src={userPicture}
-          width={30}
-          height={30}
-          className="h-8 w-8 rounded-full"
-        ></Image>
-        <div className="flex flex-col">
-          <span className="font-sans text-sm text-black/80">{userName}</span>
-          <span className="font-sans text-xs text-black/60">
-            {creationDate}
-          </span>
+    <CardBase className="md:p-4">
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <Image
+            src={userPicture}
+            width={30}
+            height={30}
+            className="h-8 w-8 rounded-full"
+          ></Image>
+          <div className="flex flex-col">
+            <span className="font-sans text-sm text-black/80">{userName}</span>
+            <span className="font-sans text-xs text-black/60">
+              {creationDate}
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-1">
+          <Button
+            className="!p-1.5"
+            onClick={handleEdit}
+            type="button"
+            variant="text"
+          >
+            <PencilSquareIcon width={20} height={20} />
+          </Button>
+          <Button
+            className="!p-1.5"
+            onClick={handleClose}
+            type="button"
+            variant="text"
+          >
+            <XMarkIcon width={20} height={20} />
+          </Button>
         </div>
       </div>
       <div className="font-sans text-xl font-bold hover:text-blue-700 md:text-2xl">
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,50 +1,29 @@
-import CardBase from "./CardBase";
 import Post from "./Post";
-import { useEffect, useState } from "react";
+import { usePosts } from "@/hooks/usePosts";
+
 export default function Posts() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const endpoint = "http://localhost:8080/post";
-        const response = await fetch(endpoint, {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },
-        });
-        const data = await response.json();
-        if (!data.success) {
-          console.error("Error en el fetch de la base de datos");
-        }
-        const posts = data.data.posts;
-        setPosts(posts);
-      } catch (error) {
-        console.error("Error en el fetch" || error);
-      }
-    };
-    fetchPost();
-  }, []);
-
+  const { results, onDelete } = usePosts();
   return (
     <div className="flex flex-col gap-4">
-      {posts ? (
-        posts.map((post, index) => {
+      {results ? (
+        results.map((post, index) => {
           return (
-            <CardBase key={index}>
-              <Post
-                creationDate={"Apr 3"}
-                userPicture={
-                  post.user.profilePic ||
-                  "https://media2.dev.to/dynamic/image/width=775%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwkz8yrhhodumbj7pnfwv.png"
-                }
-                userName={post.user.name}
-                mainText={
-                  post.title ||
-                  "Announcing the Pulumi Deploy and Document Challenge: $3,000 in Prizes!"
-                }
-                body={post.body}
-              ></Post>
-            </CardBase>
+            <Post
+              onDelete={onDelete}
+              postID={post._id}
+              key={index}
+              creationDate={"Apr 3"}
+              userPicture={
+                post.user.profilePic ||
+                "https://media2.dev.to/dynamic/image/width=775%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwkz8yrhhodumbj7pnfwv.png"
+              }
+              userName={post.user.name}
+              mainText={
+                post.title ||
+                "Announcing the Pulumi Deploy and Document Challenge: $3,000 in Prizes!"
+              }
+              body={post.body}
+            ></Post>
           );
         })
       ) : (
